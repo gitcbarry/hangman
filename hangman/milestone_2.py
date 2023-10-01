8a5a2165e1a0852293beb448d7aa21abd268e8dd
Chris Barry <cbarry@hep.ph.liv.ac.uk>
2023-10-01 21:50:01 +0100

    Add file with word list and some basic error checking

---
+++ hangman/milestone_2.py
@@ -0,0 +1,16 @@
+import random
+word_list = ["apple", "pear", "banana", "satsuma", "mango"]
+print(word_list)
+for i in range(0,5):
+  word = random.choice(word_list)
+  print(word)
+guess = input("please input a letter ")  
+if len(guess) == 1 and guess.isalpha():
+  print("Good guess")
+elif len(guess) != 1:
+  print("Oops! That is not a valid input. Input is greater than one letter")
+  raise ValueError
+elif guess.isalpha() == False:
+  print("Oops! That is not a valid input. Input is not a letter")  
+  raise TypeError
+print(guess)
